214d2cb5eaf7e4c15ccb498901c6d69ed71e6e9d
Elizaveta Strelnikova <elizaveta.strelnikova357@gmail.com>
2025-04-13 18:41:20 +0300

    add tests

---
+++ AreaLibraryTests/AreaLibraryTests.cs
@@ -0,0 +1,37 @@
+﻿using AreaLibrary.Models;
+using FluentAssertions;
+using Xunit;
+
+namespace AreaLibrary.AreaLibraryTests
+{
+    public class AreaLibraryTests
+    {
+        [Fact]
+        public void AreaCircle_Test()
+        {
+            var r = 5;
+            var expectedArea = 78.53981633974483;
+
+            var circle = new Circle(r);
+            var area = circle.Area();
+
+            area.Should().BeApproximately(expectedArea, 0.0001);
+
+        }
+
+        [Fact]
+        public void AreaTriangle_Test()
+        {
+            var a = 15;
+            var b = 10;
+            var c = 15;
+            var expectedArea = 70.71067811865476;
+
+            var triangle = new Triangle(a,b,c);
+            var area = triangle.Area();
+
+            area.Should().BeApproximately(expectedArea, 0.0001);
+
+        }
+    }
+}
